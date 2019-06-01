0a3d4d9d172fc1a5df8eea9dba23c7b1b9dd99fd
xyzlions <47937465+xyzlions@users.noreply.github.com>
2019-06-01 21:03:10 +0800

    input type email

--- assets/js/header.js
+++ assets/js/header.js
@@ -56,7 +56,7 @@ document.write("<img src=\"assets/img/1.png\"style=\"width:130px;\" alt=\"Avatar
 document.write("</div>");
 document.write("<div class=\"container\">");
 document.write("<h5 style=\"text-align:left;font-weight:bold;\">電子信箱</h5>");
-document.write("<input id=\"input\" class=\"text\" type=\"mail\" placeholder=\"Enter E-mail\" name=\"uname\" required>");
+document.write("<input id=\"input\" class=\"text\" type=\"email\" placeholder=\"Enter E-mail\" name=\"uname\" required>");
 document.write("<h5 style=\"text-align:left;font-weight:bold;\">密碼</h5>");
 document.write("<input type=\"password\" class=\"psw\" placeholder=\"Enter Password\" name=\"psw\" required>");
 document.write("<button id=\"btn\" type=\"submit\">Login</button>");
@@ -77,7 +77,7 @@ document.write("<div class=\"container\">");
 document.write("<h5 style=\"text-align:left;font-weight:bold;\">使用者名稱</h5>");
 document.write("<input id=\"input\"  type=\"text\" class=\"text\" placeholder=\"Enter Username\" name=\"uname\" required>");
 document.write("<h5 style=\"text-align:left;font-weight:bold;\">電子信箱</h5>");
-document.write("<input type=\"mail\" class=\"text\" placeholder=\"Enter E-mail\" name=\"mail\" required>");
+document.write("<input type=\"email\" class=\"text\" placeholder=\"Enter E-mail\" name=\"mail\" required>");
 document.write("<h5 style=\"text-align:left;font-weight:bold;\">密碼</h5>");
 document.write("<input type=\"password\" class=\"psw\" placeholder=\"Enter Password\" name=\"psw\" required>");
 document.write("<h5 style=\"text-align:left;font-weight:bold;\">電話</h5>");
